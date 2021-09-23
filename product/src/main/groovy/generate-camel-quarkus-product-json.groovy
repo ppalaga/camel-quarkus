203a4471960a6efa3e3d95d4d0cde0177f0a442c
Peter Palaga <ppalaga@redhat.com>
2021-09-23 17:24:03 +0200

    Productize further extensions required by Camel K, set support levels

--- product/src/main/groovy/generate-camel-quarkus-product-json.groovy
+++ product/src/main/groovy/generate-camel-quarkus-product-json.groovy
@@ -26,21 +26,45 @@ final Path basePath = project.basedir.toPath()
 final JsonSlurper jsonSlurper = new JsonSlurper()
 def productSourceJson = jsonSlurper.parse(basePath.resolve('src/main/resources/camel-quarkus-product-source.json'))
 def extensions = productSourceJson.extensions
-println "extensions = " + extensions
+// println "extensions = " + extensions
 
 def productJson = [:]
 
 productJson.extensions = []
 
 extensions.each { k, v ->
-    productJson.extensions << [
-        "group-id": "org.apache.camel.quarkus",
-        "artifact-id": k,
-        "metadata" : [
-            "redhat-support" : [ "supported" ]
+    final String quarkusSupportLevel = CqSupportStatus.valueOf(v.jvm).mergeForQuarkus(CqSupportStatus.valueOf(v.native))
+    if (quarkusSupportLevel != null) {
+        productJson.extensions << [
+            "group-id": "org.apache.camel.quarkus",
+            "artifact-id": k,
+            "metadata" : [
+                "redhat-support" : [ quarkusSupportLevel ]
+            ]
         ]
-    ]
+    }
 }
 final Path productJsonPath = basePath.resolve('target/camel-quarkus-product.json')
 Files.createDirectories(productJsonPath.getParent())
 Files.write(productJsonPath, JsonOutput.prettyPrint(JsonOutput.toJson(productJson)).getBytes('UTF-8'))
+
+
+enum CqSupportStatus {
+    community(null), techPreview('tech-preview'), supported('supported');
+    private final quarkusSupportLevel;
+    private CqSupportStatus(String quarkusSupportLevel) {
+        this.quarkusSupportLevel = quarkusSupportLevel;
+    }
+    public String mergeForQuarkus(CqSupportStatus native_) {
+        if (native_ == this) {
+            return quarkusSupportLevel;
+        }
+        if (this == CqSupportStatus.supported && native_ == CqSupportStatus.techPreview) {
+            return 'supported-in-jvm'
+        }
+        if (this == CqSupportStatus.techPreview && native_ == CqSupportStatus.community) {
+            return CqSupportStatus.community.quarkusSupportLevel
+        }
+        throw new IllegalStateException("Cannot merge native support level " + native_ + " with JVM supportlevel " + this);
+    }
+}
